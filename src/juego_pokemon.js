1607526e2c5a745b4c02209734ca3ffb73271857
Juan Manuel Androetto <juan.androetto36@gmail.com>
2022-01-10 19:50:34 +0100

    Avance en el codigo de la batalla

--- src/juego_pokemon.js
+++ src/juego_pokemon.js
@@ -1,12 +1,44 @@
 import Batalla from "./batalla.js";
-import Jugador from "./jugador.js";
-import Pokemon from "./pokemon.js";
 
-//Crear jugadores
+const batalla = new Batalla();
 
-//Mostrar Pokemones elegidos y pokemones del rival
+while (!batalla.juegoTerminado) {
+  batalla.saludoBienvenida();
+  await batalla.inicioBatalla();
 
-//Inicio del juego
-//Empieza la pelea
-//Ambos jugadores eligen el pokemon con el que se empezara a pelear
+  //Crear jugadores
+  await batalla.crearJugadores();
+
+  //Mostrar Pokemones elegidos y pokemones del rival
+  batalla.mostrarPokemones();
+
+  //Ambos jugadores eligen el pokemon con el que se empezara a pelear
+  await batalla.jugador1.elegirPokemon();
+  await batalla.jugador2.elegirPokemon();
+
+  //Empieza la pelea
+  while (!batalla.combateTerminado) {
+    // Creo variables del jugador actual y del oponente
+    const playerActual = undefined;
+    const oponente = undefined;
+    if (batalla.turnoActual === 1) {
+      playerActual = batalla.jugador1;
+      oponente = batalla.jugador2;
+    } else {
+      playerActual = batalla.jugador2;
+      oponente = batalla.jugador1;
+    }
+    batalla.mostrarJugadorActual();
+
+    // Si el pokemon actual muerte tiene que ser remplazado
+    if (!playerActual.pokemonActual) {
+      await playerActual.elegirPokemon();
+    }
+    // Se elige el ataque que se va a usar o cambiar de pokemon
+    await batalla.playerActual.elegirAtaque();
+
+    // Se ejecuta el ataque
+    batalla.playerActual.atacar(oponente);
+  }
+}
 
